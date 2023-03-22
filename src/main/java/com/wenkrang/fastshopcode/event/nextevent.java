1e1f34119679225a69bca90a47ff069405fc6a27
Wenkrangha <2934356978@qq.com>
2023-03-22 21:29:06 +0800

    event!

---
+++ src/main/java/com/wenkrang/fastshopcode/event/nextevent.java
@@ -0,0 +1,16 @@
+package com.wenkrang.fastshopcode.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+//这个类专门处理销售后的下一步界面事件处理 =D
+public class nextevent implements Listener {
+    @EventHandler
+    public void nextevent(InventoryClickEvent event) {
+        //事件处理
+        //点击事件 二
+        //TODO:
+        //事件ID:A001
+    }
+}
